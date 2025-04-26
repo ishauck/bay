0351755a2f8ac60cb24cf6ff6a7f5966f01206a6
ishauck <98329201+ishauck@users.noreply.github.com>
2025-04-26 15:17:08 -0700

    fix(auth): Add suspense boundary

--- apps/central/app/(auth)/login/page.tsx
+++ apps/central/app/(auth)/login/page.tsx
@@ -1,12 +1,22 @@
 import Logo from "@/components/logo";
 import LoginButton from "./login-button";
+import { Suspense } from "react";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
 
 export default function Login() {
     return (
         <div className="w-full h-1/2 md:w-1/2 md:h-full flex items-center justify-center flex-col p-3">
             <Logo className="w-10 h-10 text-foreground" />
             <h1 className="text-4xl font-bold mb-4">Welcome Back</h1>
-            <LoginButton />
+            <Suspense fallback={(
+                <Button disabled>
+                    <Loader2 className="w-4 h-4 mr-2 animate-spin" />
+                    Login with Github 
+                </Button>
+            )}>
+                <LoginButton />
+            </Suspense>
         </div>
     )
 }
