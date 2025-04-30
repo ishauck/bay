05097605dd00f94416c86cb9b02401c8e14dbb88
ishauck <98329201+ishauck@users.noreply.github.com>
2025-04-29 19:23:51 -0700

    refactor: Changed REDIS_KV_URL to REDIS_KV_REST_API_URL.

--- apps/central/lib/redis.ts
+++ apps/central/lib/redis.ts
@@ -2,6 +2,6 @@ import { Redis } from "@upstash/redis";
 import { env } from "./env";
 
 export const db = new Redis({
-  url: env.REDIS_KV_URL,
+  url: env.REDIS_KV_REST_API_URL,
   token: env.REDIS_KV_TOKEN,
 });
